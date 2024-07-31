97e33285be1bea07d06a69b9f2f1ed16b36492ce
Somyann <somiaprecious91@gmail.com>
2024-07-31 02:01:25 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+ const apiUrl = process.argv[2] || 'https://swapi-api.alx-tools.com/api/films/';
+const wedgeAntillesId = 18;
+
+function fetchMovies(apiUrl) {
+    request(apiUrl, { json: true }, (error, response, body) => {
+        if (error) {
+            console.error('Error fetching data:', error);
+            return;
+        } if (response.statusCode === 200 && body.results) {
+		 const moviesWithWedgeAntilles = body.results.filter(movie =>
+                movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)
+            );
+		 console.log(moviesWithWedgeAntilles.length);
+        } else {
+            console.error('Invalid response from API');
+        }
+    });
+}fetchMovies(apiUrl);
